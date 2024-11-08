032d5d03471ac18a64340602a1d14340ee66e246
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-11-08 22:19:11 +0530

    Created project for JPA and Hibernate

---
+++ learningJPAandHibernate/src/main/java/com/kartikjaiswal88/learningJPAandHibernate/LearningJpAandHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.kartikjaiswal88.learningJPAandHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpAandHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpAandHibernateApplication.class, args);
+	}
+
+}
---
+++ learningJPAandHibernate/src/test/java/com/kartikjaiswal88/learningJPAandHibernate/LearningJpAandHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartikjaiswal88.learningJPAandHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpAandHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
